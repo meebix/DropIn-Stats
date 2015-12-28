c0d7f217228a6fe7e3d5b0e4a615add0dc371e6f
Mike Donahue <mtdonahue1@gmail.com>
2015-12-28 22:16:27 +0000

    Updated event stats to compare population from user events table

--- stat-scripts/event-stats.js
+++ stat-scripts/event-stats.js
@@ -28,10 +28,11 @@ var timelineQuery = new Parse.Query(Timeline);
 // // Main iteration
 eventsQuery
 .each(function(eventObj) {
-  var todaysDate = moment(new Date()).format('MM-DD-YYYY');
-  var eventDate = moment(eventObj.attributes.eventStart).format('MM-DD-YYYY');
+  var startDay = moment().subtract(1, 'days').hours(9).minute(0).second(0).millisecond(0); // 9am yesterday UTC (4am EST)
+  var endDay = moment().hours(9).minute(0).second(0).millisecond(0); // 9am today UTC (4am EST)
+  var eventDate = moment(eventObj.attributes.eventEnd);
 
-  if (eventObj.attributes.eventStart !== undefined && eventDate === todaysDate) {
+  if (eventDate.isBetween(startDay, endDay)) {
     calcStats(eventObj);
   }
 });
@@ -43,19 +44,27 @@ function calcStats(eventObj) {
 
   usersEventsQuery.equalTo('eventId', eventObj);
   usersEventsQuery.include('userId.barId');
-  return usersEventsQuery.find().then(function(usersEventObj) {
+  return usersEventsQuery.find().then(function(usersEventObjs) {
     // Event ID
     var eventId = eventObj;
 
     // Bar ID
-    var barId = usersEventObj[0].attributes.barId;
+    var barId = usersEventObjs[0].attributes.barId;
 
     // Number of users event was sent to
-    var usersSentTo = usersEventObj.length;
+    var usersSentTo = usersEventObjs.length;
 
     // Store event start and end date for use later
     data.eventStart = eventObj.attributes.eventStart;
     data.eventEnd = eventObj.attributes.eventEnd;
+
+    // Collect user ids from population for comparison below
+    data.userIds = [];
+    _.each(usersEventObjs, function(obj) {
+      var userId = obj.attributes.userId.id;
+
+      data.userIds.push(userId);
+    });
 
     var stats = {
       calcDate: new Date(),
@@ -65,7 +74,7 @@ function calcStats(eventObj) {
     };
 
     data.stats = stats;
-    return usersEventObj;
+    return usersEventObjs;
   })
   .then(function() {
     data.stats.creditsEarned = 0;
@@ -80,14 +89,16 @@ function calcStats(eventObj) {
 
       // Did any of the users the event was sent to match a user from this subset and is date between event date
       _.each(filterByBar, function(obj) {
+        var userId = obj.attributes.userId.id;
+
         var entryDate = moment(obj.attributes.date);
         var eventStartDate = moment(data.eventStart);
         var eventEndDate = moment(data.eventEnd);
 
         // Testing
-        // console.log(data.stats.eventId.id, eventStartDate._d, eventEndDate._d, obj.attributes.userId.id, entryDate._d, entryDate.isBetween(eventStartDate, eventEndDate));
+        // console.log(data.stats.eventId.id, data.stats.barId.id, eventStartDate._d, eventEndDate._d, obj.attributes.userId.id, entryDate._d, entryDate.isBetween(eventStartDate, eventEndDate), _.contains(data.userIds, userId));
 
-        if (entryDate.isBetween(eventStartDate, eventEndDate)) {
+        if (entryDate.isBetween(eventStartDate, eventEndDate) && _.contains(data.userIds, userId)) {
           data.stats.creditsEarned++;
         }
       });
