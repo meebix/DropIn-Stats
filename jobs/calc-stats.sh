bae23d1b292b7dd40e69d73e60eacf0aee65e860
Mike Donahue <mtdonahue1@gmail.com>
2016-02-03 21:49:36 -0800

    Updated jobs files

--- jobs/calc-stats.sh
+++ jobs/calc-stats.sh
@@ -2,11 +2,8 @@
 
 cd ~/stats/stats-scripts/
 
-/usr/bin/nodejs  bar-table.js
-/usr/bin/nodejs  events-table.js
-/usr/bin/nodejs  loyalty-levels-table.js
-/usr/bin/nodejs  role-table.js
-/usr/bin/nodejs  timeline-table.js
-/usr/bin/nodejs  users-events-table.js
-/usr/bin/nodejs  users-rewards-table.js
-/usr/bin/nodejs  users-table.js
+ruby dropin-stats.rb
+ruby event-stats.rb
+ruby reward-stats.rb
+ruby traffic-stats.rb
+ruby users-stats.rb
