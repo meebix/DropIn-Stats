a2cdb35c20d3542477cf31d20952a35b0437fc37
Mike Donahue <mtdonahue1@gmail.com>
2016-03-15 22:43:49 -0700

    Updating bash jobs

--- uat/jobs/uat/calc-stats.sh
+++ uat/jobs/uat/calc-stats.sh
@@ -5,8 +5,8 @@ _environment=$1
 cd ~/stats/uat/stats-scripts/
 
 # Production
-ENV=_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-dropin-stats.rb
-ENV=_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-event-stats.rb
-ENV=_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-reward-stats.rb
-ENV=_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-traffic-stats.rb
-ENV=_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-users-stats.rb
+ENV=$_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-dropin-stats.rb
+ENV=$_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-event-stats.rb
+ENV=$_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-reward-stats.rb
+ENV=$_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-traffic-stats.rb
+ENV=$_environment /home/ubuntu/.rvm/rubies/ruby-2.2.1/bin/ruby uat-users-stats.rb
