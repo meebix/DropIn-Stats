0a83efe6b9e52d675493a231bfc4403e7e876c17
Mike Donahue <mtdonahue1@gmail.com>
2016-03-15 23:31:53 -0700

    Updating paths

--- jobs/parse-to-csv.sh
+++ jobs/parse-to-csv.sh
@@ -4,11 +4,11 @@ _environment=$1
 
 cd ~/stats/uat/mysql-scripts/
 
-ENV=$_environment /usr/bin/nodejs  bar-table.js
-ENV=$_environment /usr/bin/nodejs  events-table.js
-ENV=$_environment /usr/bin/nodejs  loyalty-levels-table.js
-ENV=$_environment /usr/bin/nodejs  role-table.js
-ENV=$_environment /usr/bin/nodejs  timeline-table.js
-ENV=$_environment /usr/bin/nodejs  users-events-table.js
-ENV=$_environment /usr/bin/nodejs  users-rewards-table.js
-ENV=$_environment /usr/bin/nodejs  users-table.js
+ENV=$_environment /usr/bin/nodejs bar-table.js
+ENV=$_environment /usr/bin/nodejs events-table.js
+ENV=$_environment /usr/bin/nodejs loyalty-levels-table.js
+ENV=$_environment /usr/bin/nodejs role-table.js
+ENV=$_environment /usr/bin/nodejs timeline-table.js
+ENV=$_environment /usr/bin/nodejs users-events-table.js
+ENV=$_environment /usr/bin/nodejs users-rewards-table.js
+ENV=$_environment /usr/bin/nodejs users-table.js
