442d8e5043a2a765d83fdb01d8a2c75b7fa6b429
Mike Donahue <mtdonahue1@gmail.com>
2016-03-15 23:24:22 -0700

    Updating env file

--- environments.js
+++ environments.js
@@ -1,15 +1,19 @@
+var ENV;
 var PARSE_ID;
 var PARSE_SECRET;
 
 if (process.env.ENV === 'production') {
+  ENV = process.env.ENV;
   PARSE_ID = process.env.PARSE_ID;
   PARSE_SECRET = process.env.PARSE_SECRET;
 } else {
+  ENV = process.env.ENV;
   PARSE_ID = process.env.UAT_PARSE_ID;
   PARSE_SECRET = process.env.UAT_PARSE_SECRET;
 }
 
 module.exports = {
+  ENV: ENV,
   PARSE_ID: PARSE_ID,
   PARSE_SECRET: PARSE_SECRET
 };
