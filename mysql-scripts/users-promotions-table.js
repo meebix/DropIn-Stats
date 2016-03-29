5fb2808f7cd3343a133fce15eb1eaaca8f7e6c14
Mike Donahue <mtdonahue1@gmail.com>
2016-03-28 22:40:51 -0700

    Trello Card 6SbVXG54: Added promotions table csv

---
+++ mysql-scripts/users-promotions-table.js
@@ -0,0 +1,100 @@
+// Users Promotions Table Dump
+
+var Parse = require('parse').Parse;
+var json2csv = require('json2csv');
+var fs = require('fs');
+var _ = require('underscore');
+var moment = require('moment');
+var env = require('../environments');
+
+// Parse Keys
+Parse.initialize(env.PARSE_ID, env.PARSE_SECRET);
+
+var UsersPromotions = Parse.Object.extend('Users_Promotions');
+
+// Data Dump
+var total;
+var iterations;
+var firstRun = true;
+var objectId = null;
+var tableData = [];
+var filename;
+var fields = [
+  'objectId',
+  'lastPromotionUsed',
+  'pendingReferralCredits',
+  'sentShares',
+  'shareCode',
+  'shareCreditReceived',
+  'totalReferralCredits',
+  'userId',
+  'createdAt',
+  'updatedAt'
+];
+
+// Filename
+if (env.ENV === 'production') {
+  filename = 'users-promotions-table.csv';
+} else {
+  filename = 'uat-users-promotions-table.csv';
+}
+
+// Query
+var usersPromotionsQuery = new Parse.Query(UsersPromotions);
+usersPromotionsQuery.count().then(function(totalRows) {
+  total = totalRows;
+  iterations = Math.ceil(total / 1000);
+})
+.then(function() {
+  var usersPromotionsQuery = new Parse.Query(UsersPromotions);
+
+  var promise = Parse.Promise.as();
+  _.times(iterations, function() {
+    promise = promise.then(function() {
+      var count = 0;
+
+      usersPromotionsQuery.include('userId');
+      usersPromotionsQuery.descending('objectId');
+      usersPromotionsQuery.limit(1000);
+      if (!firstRun) usersPromotionsQuery.lessThan('objectId', objectId);
+      return usersPromotionsQuery.find().then(function(results) {
+        _.each(results, function(obj) {
+          count = count + 1;
+
+          if (count === 1000) {
+            objectId = obj.id;
+          }
+
+          var formattedObj = {
+            objectId: obj.id,
+            lastPromotionUsed: obj.attributes.lastPromotionUsed,
+            pendingReferralCredits: obj.attributes.pendingReferralCredits,
+            sentShares: obj.attributes.sentShares,
+            shareCode: obj.attributes.shareCode,
+            shareCreditReceived: obj.attributes.shareCreditReceived,
+            totalReferralCredits: obj.attributes.totalReferralCredits,
+            userId: obj.attributes.userId ? obj.attributes.userId.id : null,
+            createdAt: obj.createdAt.toISOString(),
+            updatedAt: obj.updatedAt.toISOString()
+          };
+
+          tableData.push(formattedObj);
+        });
+      })
+      .then(function() {
+        firstRun = false;
+      });
+    });
+  });
+
+  return promise;
+})
+.then(function() {
+  json2csv({ data: tableData, fields: fields }, function(err, csv) {
+    if (err) console.log(err);
+    fs.writeFile('../csv/' + filename, csv, function(err) {
+      if (err) throw err;
+      console.log('file saved');
+    });
+  });
+});
